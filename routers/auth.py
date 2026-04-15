33f2732ebbff68285f45e582045bc5f05becffab
ncb-yji <clanic.otl@gmail.com>
2026-04-16 01:18:47 +0900

    feat: 로그인(login) 기능 추가

--- routers/auth.py
+++ routers/auth.py
@@ -44,6 +44,6 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSessi
         
     access_token_expires = timedelta(minutes=security.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = security.create_access_token(
-        data={"sub": user.email, "role": user.role}, expires_delta=access_token_expires
+        data={"sub": user.email, "role": user.role.value}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer", "role": user.role.value}
